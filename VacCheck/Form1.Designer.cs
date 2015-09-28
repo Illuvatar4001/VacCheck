f5ba17dc65fc896e42aaa26964f25558271e01c9
Illuvatar4001 <mattester@gmx.de>
2015-09-28 17:41:24 +0200

    Design

--- VacCheck/Form1.Designer.cs
+++ VacCheck/Form1.Designer.cs
@@ -31,8 +31,6 @@
             this.loaddumps = new System.Windows.Forms.Button();
             this.vacs = new System.Windows.Forms.Button();
             this.settings = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // loaddumps
@@ -62,31 +60,11 @@
             this.settings.Text = "Settings";
             this.settings.UseVisualStyleBackColor = true;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(186, 91);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 60);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "DILDOS";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(380, 99);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 51);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 183);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(539, 83);
             this.Controls.Add(this.settings);
             this.Controls.Add(this.vacs);
             this.Controls.Add(this.loaddumps);
@@ -101,8 +79,6 @@
         private System.Windows.Forms.Button loaddumps;
         private System.Windows.Forms.Button vacs;
         private System.Windows.Forms.Button settings;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
